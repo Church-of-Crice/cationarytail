248a5ce211db72736a76b50903e00aaf717424b6
Michael Eden <70781525+Deltt@users.noreply.github.com>
2024-10-16 00:40:43 +0200

    Added spawn parameter in scene changer
    - Added globals autoload to keep track of variables between scenes (currently only used to take the desired spawn location from a scene changer into the next scene and spawn the player there)
    
    - Also fixed a bug with changing scenes
    (code was correct but an existing engine bug prevented loading between 2 scenes back and forth with the previous implementation, for more info see https://github.com/godotengine/godot/issues/77090)

---
+++ project/misc/Globals.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Globals : Node
+{
+	public static Globals Instance {get; private set;}
+	
+	public Vector2 Spawn = Vector2.Zero;
+
+	public override void _Ready()
+	{
+		Instance = this;
+	}
+}

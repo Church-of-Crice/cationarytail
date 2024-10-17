6f004dc43cd6b836eba6812d87f115a1a2291f57
CruelRice <cruelrice@gmail.com>
2024-10-16 17:56:51 -0700

    play button working

--- project/ui/screens/main_menu/main_menu.gd
+++ project/ui/screens/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-@onready var start_button: Button = %StartButton
+@onready var start_button: TextureButton = %PlayButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

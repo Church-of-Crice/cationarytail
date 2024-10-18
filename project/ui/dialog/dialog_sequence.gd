57c4255c66fa7ba6f9e99442d8d29924c8c7ea23
Jonathan Lewis <jonathandavidlewis@gmail.com>
2024-10-17 21:58:18 -0700

    Add vet visit

--- project/ui/dialog/dialog_sequence.gd
+++ project/ui/dialog/dialog_sequence.gd
@@ -1,6 +1,8 @@
 class_name DialogSequence
 extends Resource
 
+signal finished
+
 @export var events: Array[DialogEvent]
 
 var current_index = 0
@@ -8,6 +10,7 @@ var current_index = 0
 func get_next():
 	if current_index >= events.size():
 		current_index = 0
+		finished.emit()
 		return null
 	var event = events[current_index]
 	current_index += 1

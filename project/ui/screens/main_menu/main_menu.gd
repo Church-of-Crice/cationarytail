7f69d78919f6b41ec1768da88ce7ea450abe9c20
TheRealDude-X <TheRealDude314@gmail.com>
2024-10-18 21:02:24 +0200

    Fix menu sounds and make quit and settings button work

--- project/ui/screens/main_menu/main_menu.gd
+++ project/ui/screens/main_menu/main_menu.gd
@@ -1,25 +1,27 @@
 extends Control
 
 @onready var start_button: TextureButton = %PlayButton
+@onready var quit_button: TextureButton = %QuitButton
+@onready var settings_button: TextureButton = %SettingsButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	start_button.pressed.connect(_on_start_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
 
 func _on_start_button_pressed() -> void:
-	# Create a new Timer node dynamically
-	var timer = Timer.new()
-	timer.wait_time = 1.0  # Set the timer to 2 seconds
-	timer.one_shot = true
-	add_child(timer)  # Add the timer to the scene
-
-	# Start the timer and wait for it to finish
-	timer.start()
-	await timer.timeout  # Pause execution until the timer times out
-
-	# Continue with whatever comes after the pause
-	print("1 seconds have passed")
-	
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_packed.call_deferred(SceneManager.CHARACTER_CREATION_SCREEN)
 	pass
+
+func _on_quit_button_pressed() -> void:
+	await get_tree().create_timer(1.0).timeout
+	get_tree().quit()
+
+func _on_settings_button_pressed() -> void:
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_packed.call_deferred(SceneManager.SETTINGS_SCREEN)
+	pass
+	

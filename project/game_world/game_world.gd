84e14d535e6c845359afaab0aa3f654e1fe788a2
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-10-20 02:18:14 -0700

    make deadly interactable only actable once

--- project/game_world/game_world.gd
+++ project/game_world/game_world.gd
@@ -5,16 +5,20 @@ var marker: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  var parent_scene = get_tree().current_scene
-  var vet_office = parent_scene.get_node("VetOffice")
+	CharacterData.player_died.connect(_on_player_died)
+	var parent_scene = get_tree().current_scene
+	var vet_office = parent_scene.get_node("VetOffice")
 
-  player = parent_scene.get_node("PlayerCharacter")
-  marker = vet_office.get_node("Marker2D")
+	player = parent_scene.get_node("PlayerCharacter")
+	marker = vet_office.get_node("Marker2D")
 
 func _input(event: InputEvent) -> void:
-  if event is InputEventKey and event.pressed:
-    if event.keycode == KEY_R:
-      respawn()
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_R:
+			respawn()
+
+func _on_player_died(death_type: CharacterData.DeathType):
+	respawn()
 
 func respawn() -> void:
-  SceneTransitionManager.change_position_with_transition(player, marker, SceneManager.FADE_TRANSITION)
+	SceneTransitionManager.change_position_with_transition(player, marker, SceneManager.FADE_TRANSITION)

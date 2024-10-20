0b553b9651b29ccc0f7163763c13711f78ff0a57
DavidH102 <david@britecit.com>
2024-10-19 21:31:34 -0400

    Refactor menu button behavior and appearance
    - Refactored the `OnButtonPressed` method to include a call to the `change_scene_to_packed` function in the `SceneManager` class.
    - Adjusted the button's scale on mouse hover to increase by 10%.
    - Changed the button's color to white with 80% opacity on mouse hover.

--- project/ui/screens/main_menu/MenuButton.cs
+++ project/ui/screens/main_menu/MenuButton.cs
@@ -26,21 +26,22 @@ public partial class MenuButton : TextureButton
         // Store the original scale and color
         _originalScale = Scale;
         _originalColor = Modulate;
-
     }
 
     private void OnButtonPressed()
     {
         _audioPlayer.Play();
         // You can call deferred scene change here if you uncomment this line
-        // get_tree().ChangeSceneToPacked(SceneManager.CHARACTER_CREATION_SCREEN);
+        // GetTree().CallDeferred("change_scene_to_packed", SceneManager.CHARACTER_CREATION_SCREEN);
     }
 
     private void OnMouseEntered()
     {
         // Make button bigger and change color
-        Scale = _originalScale * 1.08f; // Scale the button up by 20%
-        Modulate = new Color(1, 1, 6, 0.8f); // Change the button's color to be slightly transparent
+        Scale = _originalScale * 1.1f; // Scale the button up by 10%
+        
+       // Modulate = new Color(106 / 255f, 213 / 255f, 255 / 255f, 0.8f); 
+           Modulate = new Color(1, 1, 1, 0.9f); // Change the button's color to white with 80% opacity// Change the button's color to (106, 213, 255) with 80% opacity
     }
 
     private void OnMouseExited()
@@ -49,6 +50,7 @@ public partial class MenuButton : TextureButton
         Scale = _originalScale; // Reset the scale to original
         Modulate = _originalColor; // Reset the color to original
     }
+
     public override void _Process(double delta)
     {
         // No need to check and play the sound here anymore

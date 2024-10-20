a74fffa9737860e9ae2f022e880029e9f30dffa0
DeadRobotDev <99739042+DeadRobotDev@users.noreply.github.com>
2024-10-20 02:55:59 +0100

    Start implementing pterodactyl death

--- project/test/scenes/test_pterodactyl_death/pterodactyl_death.gd
+++ project/test/scenes/test_pterodactyl_death/pterodactyl_death.gd
@@ -1,4 +1,42 @@
 extends BaseDeathHandler
 
+@export var seconds_until_death: int
+
+@export var pterodactyl_sound: AudioStream
+
+var audio_player: AudioStreamPlayer;
+var timer: Timer
+
+func _ready() -> void:
+	super._ready()
+	_init_audio_player()
+	_init_timer()
+
+func _init_audio_player() -> void:
+	audio_player = AudioStreamPlayer.new()
+	add_child(audio_player)
+	audio_player.stream = pterodactyl_sound
+
+func _init_timer() -> void:
+	timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = seconds_until_death
+	timer.timeout.connect(on_death_start)
+	add_child(timer)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if timer.is_stopped() == false:
+			print("Cancelling death...")
+			timer.stop()
+
 func on_interact() -> void:
-	print("Specifics")
+	if timer.is_stopped():
+		print("Starting timer...")
+		timer.start()
+
+func on_death_start() -> void:
+	print("Pterodactyl")
+	audio_player.play()
+	# TODO: Animate pterodactyl.
+	_on_death_end()

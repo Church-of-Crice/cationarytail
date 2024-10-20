cac8fe27e1a36a143532cbb8756ef0419b23da5b
DavidH102 <david@britecit.com>
2024-10-19 21:07:01 -0400

    Update Godot editor path in project settings

--- project/ui/screens/main_menu/MenuButton.cs
+++ project/ui/screens/main_menu/MenuButton.cs
@@ -3,30 +3,54 @@ using System;
 
 public partial class MenuButton : TextureButton
 {
-	public AudioStreamPlayer _audioPlayer;
-	[Export] public AudioStream _sound;
+    private AudioStreamPlayer _audioPlayer;
+    private AudioStream _correctSound;
+    private Vector2 _originalScale;
+    private Color _originalColor;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		// Create a new AudioStreamPlayer instance for click sound
-		_audioPlayer = new AudioStreamPlayer();
-		AddChild(_audioPlayer);
+    public override void _Ready()
+    {
+        // Create a new AudioStreamPlayer instance for click sound
+        _audioPlayer = new AudioStreamPlayer();
+        AddChild(_audioPlayer);
 
-		// Preload the click sound file
-		//_sound = (AudioStream)GD.Load("res://assets/audio/effects/Transition Test 1.wav");
-		if (_sound == null)
-		{
-			throw new NullReferenceException("Click audio file not found.");
-		}
-		_audioPlayer.Stream = _sound;
+        // Preload the click sound file
+        _correctSound = (AudioStream)GD.Load("res://assets/audio/effects/Transition Test 1.wav");
+        _audioPlayer.Stream = _correctSound;
 
-		// Connect signals
-		Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
-	}
+        // Connect signals
+        Connect("pressed", new Callable(this, nameof(OnButtonPressed)));
+        Connect("mouse_entered", new Callable(this, nameof(OnMouseEntered)));
+        Connect("mouse_exited", new Callable(this, nameof(OnMouseExited)));
 
-	private void OnButtonPressed()
-	{
-		_audioPlayer.Play();
-	}
+        // Store the original scale and color
+        _originalScale = Scale;
+        _originalColor = Modulate;
+
+    }
+
+    private void OnButtonPressed()
+    {
+        _audioPlayer.Play();
+        // You can call deferred scene change here if you uncomment this line
+        // get_tree().ChangeSceneToPacked(SceneManager.CHARACTER_CREATION_SCREEN);
+    }
+
+    private void OnMouseEntered()
+    {
+        // Make button bigger and change color
+        Scale = _originalScale * 1.08f; // Scale the button up by 20%
+        Modulate = new Color(1, 1, 6, 0.8f); // Change the button's color to be slightly transparent
+    }
+
+    private void OnMouseExited()
+    {
+        // Reset the button scale and color
+        Scale = _originalScale; // Reset the scale to original
+        Modulate = _originalColor; // Reset the color to original
+    }
+    public override void _Process(double delta)
+    {
+        // No need to check and play the sound here anymore
+    }
 }
